5dc7323c817ecc7f134b19ee6c1cf8e0cc087a89
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-26 20:36:27 +0000

    exercise-3

---
+++ exercise-3.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+var colors = require('colors');
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+
+var table = new Table({
+    head: ['id', 'email', 'password', 'createdAt', 'modifiedAt']
+});
+
+function returnString(input){
+    if (typeof(input)=== "string") return input;
+    else if (typeof(input)==="number") return input;
+    else return ("Null");
+}
+
+
+connection.query("SELECT * FROM Account limit 5", function(err, rows, fields) {
+  // In this callback, rows will be all the rows of the query, in a regular array of regular objects
+  // fields is not used very often, but it will contain a listing of the columns with some metadata
+  // Here is an example usage:
+  // instantiate
+
+var elements = [];
+
+  
+  rows.forEach(function(row) {
+ 
+// table is an Array, so you can `push`, `unshift`, `splice` and friends 
+console.log("#".bold.yellow+colors.bold.yellow(row.id)+": "+ row.email+ " ("+"Password:".cyan+colors.green(returnString(row.password))+ " ,"+"createdAt: ".cyan+colors.green(colors.green(returnString(row.createdAt)))+ " ,"+"modifiedAt:".cyan+colors.green(returnString(row.modifiedAt))+")");
+  });
+
+  // This code will output lines like:
+  // #1: john@smith.com
+  // #2: abc@def.com
+  // #5: xx@yy.com
+  console.log(table.toString());
+  // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
+});
+connection.end();
+
+
